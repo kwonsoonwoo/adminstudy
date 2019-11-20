28946710339914f2e152fddb6e3f4b30ca37d090
kwonsoonwoo <zenuine1@gmail.com>
2019-11-20 14:45:24 +0900

    CH4-JPA를 통한 CRUD 배우기까지

---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,77 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.User;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public class UserRepositoryTest extends StudyApplicationTests {
+
+    // Dependency Injection (DI)
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create(){
+        // String sql = insert into user (%s, %s, %d) value (account, email, age);
+        User user = new User();
+        user.setAccount("TestUser03");
+        user.setEmail("TestUser03@gmail.com");
+        user.setPhoneNumber("010-1111-3333");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("TestUser3");
+
+        User newUser = userRepository.save(user);
+        System.out.println("newUser : " + newUser);
+    }
+
+    @Test
+    public void read(){
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser ->{
+            System.out.println("user : " + selectUser);
+            System.out.println("email : " + selectUser.getEmail());
+        });
+    }
+
+    @Test
+    @Transactional
+    public void update(){
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser ->{
+            selectUser.setAccount("PPPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+
+        });
+    }
+
+    @Test
+    @Transactional
+    public void delete(){
+        Optional<User> user = userRepository.findById(3L);
+
+        Assert.assertTrue(user.isPresent());  //false
+
+
+        user.ifPresent(selectUser->{
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(3L);
+
+        Assert.assertFalse(deleteUser.isPresent());  // false
+
+    }
+}
