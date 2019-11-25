db98cc6ba9a2518b2071335a73522d964a000028
kwonsoonwoo <zenuine1@gmail.com>
2019-11-25 15:45:23 +0900

    partner item order detail table test 02

--- src/main/java/com/example/study/model/entity/OrderDetail.java
+++ src/main/java/com/example/study/model/entity/OrderDetail.java
@@ -27,8 +27,6 @@ public class OrderDetail {
 
     private BigDecimal totalPrice;
 
-    private LocalDateTime orderAt;
-
     private LocalDateTime createdAt;
 
     private String createdBy;
@@ -37,6 +35,9 @@ public class OrderDetail {
 
     private String updatedBy;
 
+    private Long itemId;
+
+    private Long orderGroupId;
 
 
 }
